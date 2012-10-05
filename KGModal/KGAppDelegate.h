f907183dcead7a90dcebcafd3ed04bf9469226a1
David Keegan <dkeegan@signnow.com>
2012-10-05 12:47:22 -0700

    Initial Commit

---
+++ KGModal/KGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KGAppDelegate.h
+//  KGModal
+//
+//  Created by David Keegan on 10/5/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KGModal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KGModal
+//
+//  Created by David Keegan on 10/5/12.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KGAppDelegate class]));
+    }
+}
